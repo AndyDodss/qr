c6e42d7b033988f921bc4516976ac009a97204a2
Andrew Karam <36473436+AndyDodss@users.noreply.github.com>
2019-03-16 19:12:30 +0200

    first

---
+++ qr_task/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login,authenticate
+import cv2
+from pyzbar import pyzbar
+# Create your views here.
+from .video import func
+
+def index(request):
+
+    return render(request,'index.html')
+
+
+def scann(request):
+    link = func()
+    #return HttpResponse(link)
+    return redirect(str(link))
+
+
